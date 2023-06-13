ae0d1d7fd6512810322bf12464b31a535c9f60e5
VitMarKha <vitmarkha@icloud.com>
2023-06-13 23:43:14 +0300

    add button's, label in storyboard and ViewController

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    
+    private var counterNumber: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Button's
+        plusButton.layer.cornerRadius = plusButton.frame.width / 2
+        plusButton.layer.masksToBounds = true
+        plusButton.titleLabel?.font = .systemFont(ofSize: 50)
+        
+        minusButton.layer.cornerRadius = minusButton.frame.width / 2
+        minusButton.layer.masksToBounds = true
+        minusButton.titleLabel?.font = .systemFont(ofSize: 50)
     }
-
-
+    
+    @IBAction func plusActionButton(_ sender: Any) {
+        counterNumber += 1
+        counterLabel.text = counterNumber.description
+    }
+    
+    @IBAction func minusActionButton(_ sender: Any) {
+        counterNumber -= 1
+        counterLabel.text = counterNumber.description
+    }
+    
 }
 
