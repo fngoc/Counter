f2d68cf1bb38e3956e61745d7bc275939abe4324
VitMarKha <vitmarkha@icloud.com>
2023-06-14 21:46:06 +0300

    add new button and text view, add logic by task

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,13 +13,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
-    private var counterNumber: Int = 0
+    @IBOutlet weak var logText: UITextView!
+    
+    private let dateFormatter: DateFormatter = DateFormatter()
+    
+    private var counterNumber: Int = 0 {
+        willSet {
+            counterLabel.text = newValue.description
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Button's
+        //Button's config
         plusButton.layer.cornerRadius = plusButton.frame.width / 2
         plusButton.layer.masksToBounds = true
         plusButton.titleLabel?.font = .systemFont(ofSize: 50)
@@ -27,17 +36,41 @@ class ViewController: UIViewController {
         minusButton.layer.cornerRadius = minusButton.frame.width / 2
         minusButton.layer.masksToBounds = true
         minusButton.titleLabel?.font = .systemFont(ofSize: 50)
+        
+        resetButton.layer.cornerRadius = minusButton.frame.width / 2
+        resetButton.layer.masksToBounds = true
+        resetButton.titleLabel?.font = .systemFont(ofSize: 50)
+        
+        //Text config
+        logText.text = "История изменений:\n"
+        logText.isEditable = false
+        
+        //Time config
+        dateFormatter.timeStyle = .medium
+        dateFormatter.dateStyle = .medium
     }
     
     @IBAction func plusActionButton(_ sender: Any) {
         counterNumber += 1
-        counterLabel.text = counterNumber.description
+        logText.text.append("[\(getCurrentTime())]: значение изменено на +1\n")
     }
     
     @IBAction func minusActionButton(_ sender: Any) {
-        counterNumber -= 1
-        counterLabel.text = counterNumber.description
+        if (counterNumber > 0) {
+            counterNumber -= 1
+            logText.text.append("[\(getCurrentTime())]: значение изменено на -1\n")
+        } else {
+            logText.text.append("[\(getCurrentTime())]: попытка уменьшить значение счётчика ниже 0\n")
+        }
     }
     
+    @IBAction func resetActionButton(_ sender: Any) {
+        counterNumber = 0
+        logText.text.append("[\(getCurrentTime())]: значение сброшено\n")
+    }
+    
+    private func getCurrentTime() -> String {
+        let currentDateTime = Date()
+        return self.dateFormatter.string(from: currentDateTime)
+    }
 }
-
